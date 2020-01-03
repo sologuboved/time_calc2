ef9280f505c7a1951a2ee23b2a1faf65507d4cf5
sologuboved <sologuboved@gmail.com>
2020-01-03 15:55:44 +0300

    added converters

--- conversions.py
+++ conversions.py
@@ -1,73 +1,62 @@
-from datetime import datetime, timedelta
-from global_vars import *
+import datetime
 
 
-def str_to_date(raw_date, loc):
-    now = datetime.now(tz=loc)
-    if raw_date == NOW:
-        return convert(now, loc, to_utc=True)
-    order = [DAY, MONTH, YEAR]
-    date = {key: dict(zip(order, map(int, raw_date.split(DOT)))).get(key, getattr(now, key)) for key in order}
-    if date[YEAR] < 100:
-        date[YEAR] += 2000
-    return convert(datetime(**date), loc, to_utc=True)
-
-
-def str_to_lapse(raw_lapse, daywise):
-    if daywise:
-        return timedelta(days=int(raw_lapse))
-    order = [SECONDS, MINUTES, HOURS, DAYS, WEEKS]
-    return timedelta(**{key: dict(zip(order, map(int, reversed(raw_lapse.split(COLON))))).get(key, 0) for key in order})
-
-
-def str_to_dow(raw_dow):
-    if raw_dow not in ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']:
-        raise ValueError("{} is not day of week".format(raw_dow))
-    return raw_dow
-
-
-def join_datetimelet(raw_datetimelet, loc):
-    now = datetime.now(tz=loc)
-    if raw_datetimelet == NOW:
-        return convert(now, loc, to_utc=True)
-    raw_date, raw_timelet = raw_datetimelet.split()
-    date_order = [DAY, MONTH, YEAR]
-    if raw_date == NOW:
-        dict_date = dict()
+def get_year_month_day(raw_date):
+    today = datetime.datetime.today()
+    date = raw_date.split('.')
+    month, year = today.month, today.year
+    if len(date) == 3:
+        day, month, year = date
+    elif len(date) == 2:
+        day, month = date
+    elif len(date) == 1:
+        day = date[0]
     else:
-        dict_date = dict(zip(date_order, map(int, raw_date.split(DOT))))
-    date = {key: dict_date.get(key, getattr(now, key)) for key in date_order}
-    if date[YEAR] < 100:
-        date[YEAR] += 2000
-    time_order = [HOUR, MINUTE, SECOND]
-    timelet = {key: dict(zip(time_order, map(int, raw_timelet.split(COLON)))).get(key, 0) for key in time_order}
-    datetimelet = {**date, **timelet}
-    return convert(datetime(**datetimelet), loc, to_utc=True)
+        raise ValueError("Unsuitable string")
+    return [int(item) for item in (year, month, day)]
 
 
-def date_to_str(date, loc, with_dow):
-    formatter = "%d %B %Y"
-    if with_dow:
-        formatter += ", %A"
-    return convert(date, loc, to_utc=False).strftime(formatter)
-
-
-def lapse_to_days(lapse):
-    return lapse.days
-
-
-def convert(date, loc, to_utc):
-    if to_utc:
-        source, target = loc, UTC
+def get_hour_minute_second(raw_timelet):
+    timelet = raw_timelet.split(':')
+    hour = minute = 0
+    if len(timelet) == 3:
+        hour, minute, second = timelet
+    elif len(timelet) == 2:
+        minute, second = timelet
+    elif len(timelet) == 1:
+        second = timelet[0]
     else:
-        source, target = UTC, loc
-    try:
-        return source.localize(date, is_dst=None).astimezone(target)
-    except ValueError:
-        return date.astimezone(target)
+        raise ValueError("Unsuitable string")
+    return [int(item) for item in (hour, minute, second)]
 
 
-if __name__ == '__main__':
-    print(str_to_date('now', MOSCOW))
-    # print(str_to_lapse("50", True))
+def str2date(raw_date):
+    raw_date = raw_date.strip()
+    today = datetime.datetime.today()
+    if raw_date == 'today':
+        return today
+    return datetime.datetime(*get_year_month_day(raw_date))
 
+
+def str2datetimelet(raw_datetimelet):
+    raw_datetimelet = raw_datetimelet.strip()
+    if raw_datetimelet == 'now':
+        return datetime.datetime.now()
+    raw_date, raw_timelet = map(str.strip, raw_datetimelet.split())
+    year, month, day = get_year_month_day(raw_date)
+    hour, minute, second = get_hour_minute_second(raw_timelet)
+    return datetime.datetime(year, month, day, hour, minute, second)
+
+
+def str2delta(raw_delta):
+    raw_delta = raw_delta.strip()
+    hour, minute, second = get_hour_minute_second(raw_delta)
+    return datetime.timedelta(seconds=second, minutes=minute, hours=hour)
+
+
+def date2str(date):
+    return '{0:%d.%m.%Y}'.format(date)
+
+
+def datetimelet2str(datetimelet):
+    return '{0:%d.%m.%Y %H:%M:%S}'.format(datetimelet)
